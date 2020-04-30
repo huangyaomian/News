2ed04c48b365481fba2c572ad675e3019ec2cdda
Mika Huang <yaomian@qoo-app.com>
2020-04-30 17:29:55 +0800

    1

--- app/src/main/java/com/hym/news/DescActivity.java
+++ app/src/main/java/com/hym/news/DescActivity.java
@@ -1,170 +1,280 @@
 package com.hym.news;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.CookieSyncManager;
 import android.webkit.SslErrorHandler;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
+import android.webkit.WebBackForwardList;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.hym.news.loading.LoadingViewManager;
+import com.hym.news.controller.ProgressBarController;
+import com.hym.news.widget.H5NoNetView;
 
-import java.lang.reflect.Method;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DescActivity extends AppCompatActivity {
 
     @BindView(R.id.webview)
     WebView webview;
+    @BindView(R.id.desc_iv_back)
+    ImageView mDescIvBack;
+    @BindView(R.id.desc_webviewcontainer)
+    RelativeLayout mDescWebviewcontainer;
+    @BindView(R.id.desc_nonetview)
+    H5NoNetView mDescNonetview;
+    @BindView(R.id.desc_progressBar)
+    ProgressBar mDescProgressBar;
     private String url;
 
-    Activity context;
+    private Timer hideProgressBarTimer;
+    private HideBarTimeTask hideBarTimeTask;
+    private String blackUrl = "about:blank";//浏览器空白页
 
 
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desc);
         ButterKnife.bind(this);
-        // 清缓存和记录，缓存引起的白屏
-        webview.clearCache(true);
-        webview.clearHistory();
-
-
 
 
 //        url = getIntent().getStringExtra("url");
-//        url = "https://mini.eastday.com/a/200430080345029.html?qid=02263";
-        url = "http://www.juming.com/broker/?tt=0&t=tiao_chaoshi&ym=www.ttxinwen.com";
-        //创建webview的设置类，对于属性进行设置
-        WebSettings settings = webview.getSettings();
-        //设置可以访问文件
-        settings.setAllowFileAccess(true);
-        settings.setJavaScriptEnabled(true);//设置页面支持js交互
-        settings.setUseWideViewPort(true);//将图片调整到适合webview的大小
-        settings.setLoadWithOverviewMode(true);//缩放至屏幕大小
-        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//设置webview的缓存方式
-        settings.setAllowFileAccess(true);//设置可以访问文件
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);//设置可以打开新窗口
-        if(Build.VERSION.SDK_INT >= 19) {
-            settings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        url = "https://mini.eastday.com/a/200430080345029.html?qid=02263";
+//        url = "https://m.baidu.com/";
+        if (url != null) {
+            url = url.trim();
+        }
+        initView();
+        loadWebView();
+    }
+
+
+    //初始化视图
+    protected void initView() {
+        WebSettings webseting = webview.getSettings();
+
+        webseting.setSupportZoom(true);
+        webseting.setBuiltInZoomControls(true);
+        webseting.setDisplayZoomControls(false);
+        webseting.setJavaScriptEnabled(true);//设置页面支持js交互
+        webseting.setUseWideViewPort(true);//将图片调整到适合webview的大小
+        webseting.setLoadWithOverviewMode(true);//缩放至屏幕大小
+        webseting.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//设置webview的缓存方式
+        webseting.setAllowFileAccess(true);//设置可以访问文件
+        webseting.setJavaScriptCanOpenWindowsAutomatically(true);//设置可以打开新窗口
+        if (Build.VERSION.SDK_INT >= 19) {
+            webseting.setLoadsImagesAutomatically(true); //支持自动加载图片
         } else {
-            settings.setLoadsImagesAutomatically(false);
+            webseting.setLoadsImagesAutomatically(false);
         }
-        settings.setDefaultTextEncodingName("utf-8");
-        settings.setDomStorageEnabled(true);
-        settings.setBlockNetworkImage(false);
-        settings.setBlockNetworkLoads(false);
-        settings.setBuiltInZoomControls(true);
-        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        settings.setUseWideViewPort(true);
 
 
-        //Android webview 从Lollipop(5.0)开始webview默认不允许混合模式，https当中不能加载http资源，需要设置开启。
-        //处理http和https混合的问题
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        } else {
-            settings.setMixedContentMode(WebSettings.LOAD_NORMAL);
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // 允许javascript出错
-            try {
-                Method method = Class.forName("android.webkit.WebView").
-                        getMethod("setWebContentsDebuggingEnabled", Boolean.TYPE);
-                if (method != null) {
-                    method.setAccessible(true);
-                    method.invoke(null, true);
+        // 设置Web视图
+        webview.setWebViewClient(new WebViewClient());
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setDownloadListener(new DownloadListener());
+
+        mDescNonetview.setRefreshListener(new H5NoNetView.HtmlReloadListener() {
+            @Override
+            public void triggerRefresh() {
+                if (netIsAvailable()) {
+                    mDescNonetview.setVisibility(View.GONE);
+                    webview.setVisibility(View.VISIBLE);
                 }
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-       /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            webview.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }*/
-//        LoadingViewManager.with(context).setHintText("加载天气中").setAnimationStyle("BallClipRotatePulseIndicator").build();
-
-//        webview.setWebChromeClient(new WebChromeClient());
-        //默通过手机流浪器打开网址，为了鞥你改过直接通过webview打开网址，就必须设置一下操作
-        webview.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                if (url.startsWith("http://") || url.startsWith("https://")) { // 4.0以上必须要加
-                    view.loadUrl(url);
-                    return true;
-                }
-                return false;
-            }
-            @Override
-            public void onPageFinished(WebView view, String url)
-            {
-                super.onPageFinished(view, url);
-                // 加载完成
-                webview.getSettings().setLoadsImagesAutomatically(true);
-                webview.setLayerType(View.LAYER_TYPE_HARDWARE,null);
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                //开始加载
-
-            }
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-//                super.onReceivedError(view, request, error);
-                //加载失败调用
-                Log.d("hymmm","加载失败了");
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-//                super.onReceivedSslError(view, handler, error);
-                switch (error.getPrimaryError()) {
-                    case SslError.SSL_INVALID: // 校验过程遇到了bug
-                    case SslError.SSL_UNTRUSTED: // 证书有问题
-                        handler.proceed();
-                    default:
-                        handler.cancel();
-                }
+                loadWebView();
             }
         });
-
-
-
-
-        //设置加载的网址
-        webview.loadUrl(url);
-
     }
+
+
+    protected void loadWebView() {
+        if (url.startsWith("https://") || url.startsWith("http://")) {
+            //同步Cookies
+            CookieSyncManager.createInstance(this);
+            CookieSyncManager.getInstance().sync();
+            webview.loadUrl(url);
+        } else {
+            webview.loadData(url, "text/html", "UTF-8");
+        }
+    }
+
+    public boolean netIsAvailable() {
+        //检测网络是否可用
+        ConnectivityManager cwjManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cwjManager.getActiveNetworkInfo() != null && cwjManager.getActiveNetworkInfo().isAvailable();
+    }
+
+    private void runTimer(int delay) {
+        stopTimer();
+        hideProgressBarTimer = new Timer(true);
+        hideBarTimeTask = new HideBarTimeTask();
+        hideProgressBarTimer.schedule(hideBarTimeTask, delay);
+    }
+
+    private void stopTimer() {
+        if (hideBarTimeTask != null) {
+            hideBarTimeTask.cancel();
+            hideBarTimeTask = null;
+        }
+
+        if (hideProgressBarTimer != null) {
+            hideProgressBarTimer.cancel();
+            hideProgressBarTimer.purge();
+            hideProgressBarTimer = null;
+        }
+    }
+
+    private ProgressBarController progressBarController = new ProgressBarController(new ProgressBarController.ControllerListener() {
+
+        @Override
+        public void stop() {
+            runTimer(500);
+        }
+
+        @Override
+        public void setProgress(int progress) {
+            mDescProgressBar.setProgress(progress);
+        }
+
+        @Override
+        public void start() {
+            if (mDescProgressBar.getVisibility() == View.GONE) {
+                mDescProgressBar.setVisibility(View.VISIBLE);
+            }
+            stopTimer();
+        }
+
+    });
+
+    class HideBarTimeTask extends TimerTask {
+        @Override
+        public void run() {
+            Message msg = new Message();
+            msg.what = 10000;
+            webviewHandler.sendMessage(msg);
+        }
+    }
+
+    private Handler webviewHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 10000) {
+                mDescProgressBar.setVisibility(View.GONE);
+                mDescProgressBar.setProgress(0);
+            }
+        }
+    };
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && webview.canGoBack()) {
-            webview.goBack();
-            return true;
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webview != null && webview.canGoBack() && checkBackUrl(blackUrl)) {
+                webview.goBack();
+            } else {
+                webview.stopLoading();
+                finish();
+            }
+
+            return false;
         }
         return super.onKeyDown(keyCode, event);
     }
+
+    private boolean checkBackUrl(String url) {
+        WebBackForwardList mWebBackForwardList = webview.copyBackForwardList();
+        String backUrl = mWebBackForwardList.getItemAtIndex(mWebBackForwardList.getCurrentIndex() - 1).getUrl();
+        //判断是否是空白页
+        if (backUrl != null && backUrl.equalsIgnoreCase(url)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            //开始加载，显示进度
+            progressBarController.preloading();
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+
+            if (failingUrl.equalsIgnoreCase(url) == false) {
+                return;
+            }
+
+            webview.loadUrl(blackUrl);
+            //只有加载完毕才应该调用clearHistory()
+            webview.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    webview.clearHistory();
+                    mDescNonetview.setVisibility(View.VISIBLE);
+                    webview.setVisibility(View.GONE);
+                }
+            }, 500);
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            //忽略SSL证书错误检测,使用SslErrorHandler.proceed()来继续加载
+            handler.proceed();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+    }
+
+    private class WebChromeClient extends android.webkit.WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            //通知程序当前页面加载进度
+            progressBarController.setCurrentValue(newProgress);
+        }
+    }
+
+    private class DownloadListener implements android.webkit.DownloadListener {
+        @Override
+        public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+            //需要Webview开启下载监听,否则点击下载连接，没有反应
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+    }
+
+
+
 
     //销毁Webview
     @Override
@@ -179,14 +289,11 @@ public class DescActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        webview.loadUrl(url);
+
+
+
+    @OnClick(R.id.desc_iv_back)
+    public void onViewClicked() {
+        finish();
     }
-
-
-
-
-
 }
